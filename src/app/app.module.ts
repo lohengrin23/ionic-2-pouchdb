798f2571c146726d60c24864189c45cb11695cfb
Lohengrin23 <lohengrin2301@gmail.com>
2018-02-17 01:03:06 -0400

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,9 +45,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AssetsProvider,
-    AssetsProvider,
-    AssetProvider,
+    
   
   ]
 })
